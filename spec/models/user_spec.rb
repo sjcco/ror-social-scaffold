094d6ced352dbc87d1d72a72e61f7e7558b116de
CarlosC <sjcco@gmail.com>
2021-05-19 17:38:27 -0500

    Add spec for user and friendship models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name) }
+
+  it 'should have many posts' do
+    user = User.reflect_on_association(:posts)
+    expect(user.macro).to eq(:has_many)
+  end
+
+  it 'should have many comments' do
+    user = User.reflect_on_association(:comments)
+    expect(user.macro).to eq(:has_many)
+  end
+
+  it 'should have many likes' do
+    user = User.reflect_on_association(:likes)
+    expect(user.macro).to eq(:has_many)
+  end
+
+  it 'should have many friendships' do
+    user = User.reflect_on_association(:friendships)
+    expect(user.macro).to eq(:has_many)
+  end
+
+  it 'should have many pending_friendships' do
+    user = User.reflect_on_association(:pending_frienships)
+    expect(user.macro).to eq(:has_many)
+  end
+
+  
+end
