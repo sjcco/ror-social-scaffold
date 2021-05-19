0b4bf64de807d0dce6a61b6272ea9cbd5df021d5
CarlosC <sjcco@gmail.com>
2021-05-19 17:39:12 -0500

    Create Frienship model and Add relationships whith this table

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,21 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :pending_frienships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
+
+  def friends
+    friendships_i_requested = Friendship.where(user_id: id, confirmed: true).pluck(:friend_id)
+    friendships_got_invited = Friendship.where(friend_id: id, confirmed: true).pluck(:user_id)
+    ids = friendships_i_requested + friendships_got_invited
+    User.where(id: ids)
+  end
+
+  def friend_with?(user)
+    Friendship.confirmed_record?(id, user.id)
+  end
+
+  def send_invitation(user)
+    Friendship.create(friend_id: user.id)
+  end
 end
