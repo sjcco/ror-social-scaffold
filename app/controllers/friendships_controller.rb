5267b5ce5b1482e39667b646599bb844cf294d76
CarlosC <sjcco@gmail.com>
2021-05-22 01:18:37 -0500

    Add mutual friendships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -8,11 +8,14 @@ class FriendshipsController < ApplicationController
   def update
     friendship = Friendship.record(current_user.id, params[:format])
     friendship.update(confirmed: true)
+    Friendship.create(user_id: friendship.friend_id, friend_id: friendship.user_id, confirmed: true)
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
     friendship_delete = Friendship.find(params[:format])
+    friendship_pair = Friendship.where("user_id = ? AND friend_id = ?", friendship_delete.friend_id, friendship_delete.user_id).first
+    friendship_pair.destroy unless friendship_pair.nil?
     friendship_delete.destroy
     redirect_back(fallback_location: root_path)
   end
